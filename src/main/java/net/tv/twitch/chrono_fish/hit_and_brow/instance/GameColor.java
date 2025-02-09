f5ead4f9a0b9c9457f33608d0ba00061027895f8
Daka-fish <yamatohidaka24@gmail.com>
2025-02-09 20:45:19 +0900

    remove useless value

--- src/main/java/net/tv/twitch/chrono_fish/hit_and_brow/instance/GameColor.java
+++ src/main/java/net/tv/twitch/chrono_fish/hit_and_brow/instance/GameColor.java
@@ -4,18 +4,18 @@ import org.bukkit.Material;
 
 public enum GameColor {
 
-    BLACK("Black","§0■",Material.BLACK_WOOL),
-    WHITE("White","§f■",Material.WHITE_WOOL),
-    RED("Red","§c■",Material.RED_WOOL),
-    BLUE("Blue","§b■",Material.LIGHT_BLUE_WOOL),
-    GREEN("Green","§a■",Material.LIME_WOOL),
-    PINK("Pink","§d■",Material.PINK_WOOL),
-    YELLOW("Yellow","§e■",Material.YELLOW_WOOL);
+    BLACK("§0■",Material.BLACK_WOOL),
+    WHITE("§f■",Material.WHITE_WOOL),
+    RED("§c■",Material.RED_WOOL),
+    BLUE("§b■",Material.LIGHT_BLUE_WOOL),
+    GREEN("§a■",Material.LIME_WOOL),
+    PINK("§d■",Material.PINK_WOOL),
+    YELLOW("§e■",Material.YELLOW_WOOL);
 
     private final String colorBlock;
     private final Material material;
 
-    GameColor(String name, String colorBlock, Material material){
+    GameColor(String colorBlock, Material material){
         this.colorBlock = colorBlock;
         this.material = material;
     }
